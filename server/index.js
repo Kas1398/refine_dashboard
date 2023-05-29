613990b8eb7068ff87cc957487b897a0a7ce0ac7
kassahun <kassuhan@gmail.com>
2023-05-29 11:09:27 +0300

    fixing deployment

--- server/index.js
+++ server/index.js
@@ -23,8 +23,8 @@ const startServer = async () => {
     try {
         connectDB(process.env.MONGODB_URL);
 
-        app.listen(8080, () =>
-            console.log("Server started on port http://localhost:8080"),
+        app.listen(8081, () =>
+            console.log("Server started on port http://localhost:8081"),
         );
     } catch (error) {
         console.log(error);
